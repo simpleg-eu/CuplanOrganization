de3ec0a33dd9fbf1ec782e1c0c676b9966b962ed
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-01 23:49:33 +0100

    d fields

--- Organization/Core/OrganizationIdentity.cs
+++ Organization/Core/OrganizationIdentity.cs
@@ -5,9 +5,24 @@ namespace Organization.Core;
 /// </summary>
 public class OrganizationIdentity
 {
+    /// <summary>
+    /// Unique identifier of the organization.
+    /// </summary>
     public string Id { get; set; }
+    /// <summary>
+    /// Name of the organization.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Address of the organization.
+    /// </summary>
     public Address Address { get; set; }
+    /// <summary>
+    /// Contact methods of the organization.
+    /// </summary>
     public IEnumerable<ContactMethod> ContactMethods { get; set; }
+    /// <summary>
+    /// Links of the organization.
+    /// </summary>
     public IEnumerable<Link> Links { get; set; }
 }
