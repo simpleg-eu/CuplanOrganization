2074abb6975bddeb605499b550b6d110ca2dda2b
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-01 23:28:55 +0100

    + `nullable` specification

--- Organization/Api/ContactType.cs
+++ Organization/Api/ContactType.cs
@@ -9,7 +9,7 @@ public class ContactType : ObjectGraphType<Contact>
     {
         Name = "Contact";
         Description = "An organization's contact method, i.e. <i>Phone, '+3464000000'.";
-        Field(c => c.Method).Description("Contact method, i.e. 'Phone'.");
-        Field(c => c.Value).Description("Contact value, i.e. '+3464000000'.");
+        Field(c => c.Method, nullable: false).Description("Contact method, i.e. 'Phone'.");
+        Field(c => c.Value, nullable: false).Description("Contact value, i.e. '+3464000000'.");
     }
 }
