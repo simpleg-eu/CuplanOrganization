d4b0a332254e30720511a42070ee76f287dc906f
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-01 23:24:08 +0100

    d fields

--- Organization/Core/Link.cs
+++ Organization/Core/Link.cs
@@ -5,6 +5,12 @@ namespace Organization.Core;
 /// </summary>
 public class Link
 {
+    /// <summary>
+    /// Name of the link, i.e. <i>Website</i>.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// The link itself, i.e. <i>https://domain.com</i>.
+    /// </summary>
     public string Value { get; set; }
 }
