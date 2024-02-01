5eaeeeb70534689c43310d4dc43f392441a3e3af
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-01 23:38:22 +0100

    d `id`, `roles` and `permissions` fields

--- Organization/Core/OrganizationPolicy.cs
+++ Organization/Core/OrganizationPolicy.cs
@@ -5,7 +5,16 @@ namespace Organization.Core;
 /// </summary>
 public class OrganizationPolicy
 {
+    /// <summary>
+    /// Unique identifier of the organization.
+    /// </summary>
     public string Id { get; set; }
+    /// <summary>
+    /// Roles available within the organization, without taking into account the default roles.
+    /// </summary>
     public IEnumerable<Role> Roles { get; set; }
+    /// <summary>
+    /// Permissions of the organization, without taking into account the default organization permissions.
+    /// </summary>
     public IEnumerable<string> Permissions { get; set; }
 }
