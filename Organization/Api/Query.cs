3566366c65694b2b021c11dbf8247adbd4c3632d
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-02 01:39:39 +0100

    + switch to `Hot Chocolate GraphQL`

--- Organization/Api/Query.cs
+++ Organization/Api/Query.cs
@@ -1,14 +1,29 @@
-using GraphQL.Types;
+using Organization.Core;
 
 namespace Organization.Api;
 
 /// <summary>
 /// Root GraphQL query type.
 /// </summary>
-public class Query : ObjectGraphType
+public class Query
 {
-    public Query()
+    public OrganizationIdentity GetOrganizationIdentity(string id)
     {
-        
+        return new OrganizationIdentity
+        {
+            Id = id,
+            Name = "Le organization",
+            Address = new Address
+            {
+                Country = "ES",
+                Region = "Albacete",
+                City = "Villarrobledo",
+                Street = "Calle Dos de Mayo",
+                Number = "2A",
+                PostalCode = "02600"
+            },
+            ContactMethods = new List<ContactMethod>(),
+            Links = new List<Link>()
+        };
     }
 }
--- Organization/Program.cs
+++ Organization/Program.cs
@@ -1,11 +1,16 @@
-using GraphQL.Server.Ui.GraphiQL;
+using Organization.Api;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
-    app.UseGraphQLGraphiQL("/graphiql");
 }
 
 app.UseHttpsRedirection();
+
+app.MapGraphQL();
+
+app.Run();
