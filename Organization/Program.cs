7e4ab695b513cb399dc0d73035b84256c217d506
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-03-12 19:03:32 +0100

    + basic configuration of GraphQL server

--- Organization/Program.cs
+++ Organization/Program.cs
@@ -1,16 +1,27 @@
+using HotChocolate.AspNetCore;
 using Organization.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddGraphQLServer().AddQueryType<Query>();
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) {
-}
-
 app.UseHttpsRedirection();
 
-app.MapGraphQL();
+if (app.Environment.IsDevelopment()) {
+    app.MapGraphQL();
+}
+else
+{
+    app.MapGraphQL().WithOptions(new GraphQLServerOptions()
+    {
+        Tool = { Enable = false },
+        EnableGetRequests = false
+    });
+}
 
 app.Run();
